b4695f6bb0760903b22ae295b48338944f5062ac
Gary Weiss <fourvelocity@gmail.com>
2017-05-15 15:45:18 +0300

    add subdomain for matchmaking

--- src/client/clientEntryPoint.js
+++ src/client/clientEntryPoint.js
@@ -6,7 +6,8 @@ require('../../dist/resources/sass/main.scss');
 
 // the official Lance.gg sprocketleague uses a matchmaker
 let matchmaker = null;
-if (window.location.hostname === 'sprocketleagueus.lance.gg')
+if (window.location.hostname === 'sprocketleagueus.lance.gg' ||
+    window.location.hostname === 'sprocketleague.lance.gg')
     matchmaker = 'http://srv.lance.gg/sprocketleagueus.lance.gg/matchmaker';
 
 // default options, overwritten by query-string options
