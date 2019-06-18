a7d3c24a861d2be8fada5c007cca4b27d803b989
namel <fourvelocity@gmail.com>
2019-06-18 10:58:15 +0300

    latest lance code - appspec changes

--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
 cd /var/games/sprocketleague
-PORT=3003 npm start >sprocketleague.out 2>sprocketleague.err &
+
+# arg 1: game name
+# arg 2: port name
+run-game () {
+    pgrep node | xargs pwdx | grep $1 |awk '{print $1}' | sed s/://g > /tmp/killpid
+    kill -9 `cat /tmp/killpid` || true
+    cd /var/games/$1
+    PORT=$2 npm start >$1.out 2>$1.err &
+}
+
+run-game sprocketleague 3003
